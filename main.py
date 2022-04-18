7e0329996dd7215504e01f3b549045e46d0bff70
wardgeronimussmets <81919921+wardgeronimussmets@users.noreply.github.com>
2022-04-18 22:30:06 +0200

    Receive all the submissions correctly

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import praw
+import pickle
+import time
+pickleFile = "myPickleFile.pk"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def storeUTC():
+    with open(pickleFile, 'wb+') as pick:
+        pickle.dump(0, pick)
+        #pickle.dump(int(time.time()), pick)
+    return
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def loadUTC():
+    with open(pickleFile, 'rb') as pick:
+        try:
+            return pickle.load(pick)
+        except EOFError:
+            return 0
+
+def checkSubmission(submission):
+    
+    return
+
+def getFromReddit():
+    lastCheckTime = loadUTC()
+    print(lastCheckTime)
+    reddit = praw.Reddit(client_id="KrKvK25tPXyhsuYKURx4aA", client_secret="WFLyQT2FYaVau7pDTUwlKCupDF2wqQ",
+                         user_agent="python:praw:gameDealsParser (by /u/dewarden)")
+    reddit.read_only = True
+    for submissions in reddit.subreddit("GameDeals").new(limit=10):
+        if submissions.created_utc > lastCheckTime:
+            #file hasn't been checked before
+            checkSubmission(submissions)
+        else:
+            #checked all new ones
+            print("Checked all the new ones")
+            return
+    #stopped checking because passed limit
+    print("There were new submissions that haven't been checked yet")
+    return
+
+def main():
+    getFromReddit()
+    storeUTC()
+    return
+
+
+main()
+
+exit(0)
+
+
+#docs: https://praw.readthedocs.io/en/stable/code_overview/models/submission.html?highlight=submission#praw.models.Submission
